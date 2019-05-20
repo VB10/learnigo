c8ae8bbef7ab87bc5604f8583c000c0e0101680c
Veli Bacık <velibacik60@gmail.com>
2019-05-21 01:52:06 +0300

    google signin ios add

---
+++ lib/src/ui/screen/login/login.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class GoogleLoginScreen extends StatefulWidget {
+  const GoogleLoginScreen({Key key}) : super(key: key);
+
+  @override
+  _GoogleLoginScreenState createState() => _GoogleLoginScreenState();
+}
+
+final GoogleSignIn _googleSignIn = GoogleSignIn();
+final FirebaseAuth _auth = FirebaseAuth.instance;
+
+class _GoogleLoginScreenState extends State<GoogleLoginScreen> {
+  Future<FirebaseUser> _handleSignIn() async {
+    final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser.authentication;
+
+    final AuthCredential credential = GoogleAuthProvider.getCredential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    final FirebaseUser user = await _auth.signInWithCredential(credential);
+    print("signed in " + user.displayName);
+    return user;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Center(
+        child: RaisedButton(
+          child: Icon(Icons.games),
+          onPressed: this._handleSignIn,
+        ),
+      ),
+    );
+  }
+}
