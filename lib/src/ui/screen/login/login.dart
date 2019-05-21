e91aec7d6498d95636fdb85fda66b47a4cf229da
Veli Bacık <velibacik60@gmail.com>
2019-05-22 01:35:00 +0300

    androidx fix and google signin add

--- lib/src/ui/screen/login/login.dart
+++ lib/src/ui/screen/login/login.dart
@@ -14,18 +14,22 @@ final FirebaseAuth _auth = FirebaseAuth.instance;
 
 class _GoogleLoginScreenState extends State<GoogleLoginScreen> {
   Future<FirebaseUser> _handleSignIn() async {
-    final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
-    final GoogleSignInAuthentication googleAuth =
-        await googleUser.authentication;
+    try {
+      final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+      final GoogleSignInAuthentication googleAuth =
+          await googleUser.authentication;
 
-    final AuthCredential credential = GoogleAuthProvider.getCredential(
-      accessToken: googleAuth.accessToken,
-      idToken: googleAuth.idToken,
-    );
+      final AuthCredential credential = GoogleAuthProvider.getCredential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
 
-    final FirebaseUser user = await _auth.signInWithCredential(credential);
-    print("signed in " + user.displayName);
-    return user;
+      final FirebaseUser user = await _auth.signInWithCredential(credential);
+      print("signed in " + user.displayName);
+      return user;
+    } catch (e) {
+      print(e);
+    }
   }
 
   @override
